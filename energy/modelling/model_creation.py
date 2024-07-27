d2e24b4bd982ad6c4fdc0b96e2f4722aff72a318
lzangari <lucazangari.93@gmail.com>
2024-07-27 20:59:29 +0200

    add hp to the model build

--- energy/modelling/model_creation.py
+++ energy/modelling/model_creation.py
@@ -2,68 +2,73 @@
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dense, Dropout, Input
 from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.layers import LeakyReLU
+from tensorflow.keras.regularizers import l2
 
 
-# Naive LSTM model
-def build_naive_lstm_model(input_shape, output_size, units=50, dropout=0.2, clip=True):
-    """Builds a naive LSTM model with a single LSTM layer and a single Dense layer.
+# LSTM-based model
+def build_lstm_based_model(
+    model_type,
+    input_shape,
+    output_size,
+    pred_length,
+    units=50,
+    dropout=0.2,
+    learning_rate=0.005,
+    alpha=0.1,
+):
+    if model_type == "lstm_naive":
+        model = Sequential()
+        model.add(Input(shape=input_shape))
+        model.add(LSTM(units=units, activation="relu"))
+        model.add(Dropout(dropout))
+        model.add(Dense(pred_length * output_size))
 
-    Args:
-        input_shape (int): input shape of the model.
-        output_size (int): output size of the model.
-        units (int, optional): the number of units in the LSTM layer. Defaults to 50.
-        dropout (float, optional): the dropout rate. Defaults to 0.2.
-        clip (bool, optional): whether to use gradient clipping. Defaults to True.
+    elif model_type == "lstm_stacked":
+        model = Sequential()
+        model.add(Input(shape=input_shape))
+        model.add(LSTM(units=units, return_sequences=True))
+        model.add(LSTM(units=units, kernel_regularizer=l2(1e-4)))
+        model.add(LeakyReLU(negative_slope=alpha))
+        model.add(Dropout(dropout))
+        model.add(Dense(pred_length * output_size))
 
-    Returns:
-        Sequential: the compiled LSTM model.
-    """
-    model = Sequential()
-    model.add(Input(shape=input_shape))
-    model.add(LSTM(units=units, activation="relu"))
-    model.add(Dropout(dropout))
-    model.add(Dense(output_size))
-
-    if clip:
-        # Compile with gradient clipping
-        optimizer = Adam(clipvalue=1.0)
-        model.compile(optimizer=optimizer, loss="mse")
-    else:
-        model.compile(optimizer="adam", loss="mse")
+    optimizer = Adam(learning_rate=learning_rate, clipvalue=1.0)
+    model.compile(optimizer=optimizer, loss="mse")
 
     return model
 
 
-# Stacked LSTM model
-def build_stacked_lstm_model(
-    input_shape, output_size, units=50, dropout=0.2, clip=True
+# Hyperparameter tuning for LSTM-based model
+def build_lstm_based_model_with_hp(
+    hp, model_type, input_shape, output_size, pred_length
 ):
-    """Builds a stacked LSTM model with two LSTM layers and a single Dense layer.
+    units = hp.Int("units", min_value=30, max_value=240, step=30)
+    dropout = hp.Float("dropout", min_value=0.1, max_value=0.25, step=0.05)
+    learning_rate = hp.Float(
+        "learning_rate", min_value=0.001, max_value=0.03, step=0.002
+    )
+    if model_type == "lstm_stacked":
+        alpha = hp.Float("alpha", min_value=0.1, max_value=0.3, step=0.1)
 
-    Args:
-        input_shape (int): input shape of the model.
-        output_size (int): output size of the model.
-        units (int, optional): the number of units in the LSTM layer. Defaults to 50.
-        dropout (float, optional): the dropout rate. Defaults to 0.2.
-        clip (bool, optional): whether to use gradient clipping. Defaults to True.
+    if model_type == "lstm_naive":
+        model = Sequential()
+        model.add(Input(shape=input_shape))
+        model.add(LSTM(units=units, activation="relu"))
+        model.add(Dropout(dropout))
+        model.add(Dense(pred_length * output_size))
 
-    Returns:
-        Sequential: the compiled LSTM model.
-    """
-    model = Sequential()
-    model.add(Input(shape=input_shape))
-    model.add(LSTM(units=units, activation="relu", return_sequences=True))
-    model.add(Dropout(dropout))
-    model.add(LSTM(units=units, activation="relu"))
-    model.add(Dropout(dropout))
-    model.add(Dense(output_size))
+    elif model_type == "lstm_stacked":
+        model = Sequential()
+        model.add(Input(shape=input_shape))
+        model.add(LSTM(units=units, return_sequences=True))
+        model.add(LSTM(units=units, kernel_regularizer=l2(1e-4)))
+        model.add(LeakyReLU(negative_slope=alpha))
+        model.add(Dropout(dropout))
+        model.add(Dense(pred_length * output_size))
 
-    if clip:
-        # Compile with gradient clipping
-        optimizer = Adam(clipvalue=1.0)
-        model.compile(optimizer=optimizer, loss="mse")
-    else:
-        model.compile(optimizer="adam", loss="mse")
+    optimizer = Adam(learning_rate=learning_rate, clipvalue=1.0)
+    model.compile(optimizer=optimizer, loss="mse")
 
     return model
 
