72ad698a239fc88c03e935d08d13dd797837abf5
lzangari <lucazangari.93@gmail.com>
2024-07-27 15:50:13 +0200

    create encoding function in utils and add data processing to the main file

--- energy/utils/utils.py
+++ energy/utils/utils.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 def convert_to_float(data):
     """Converts a string to a float, replacing commas with dots.
 
@@ -7,8 +10,10 @@ def convert_to_float(data):
     Returns:
         float: The float value of the string, or None if it can't be converted.
     """
-    if data.isinstance(float):
+    # if data is instance of float, return data
+    if isinstance(data, float):
         return data
+
     try:
         return float(data.replace(".", "").replace(",", "."))
 
@@ -17,3 +22,20 @@ def convert_to_float(data):
             return None
         else:
             print(f"Error converting {data} to float: {e}")
+
+
+# include sine and cosine transformations for the hour and day of the week
+def encode_feature(data, column_name: str, max_value: int):
+    """Encodes a feature in a way that captures the periodicity of the data.
+
+    Args:
+        data (dataframe): the dataframe containing the data to be transformed.
+        column_name (str): the name of the column to be transformed.
+        max_value (int): the maximum value of the feature to be transformed.
+
+    Returns:
+        _type_: _description_
+    """
+    data[column_name + "_sin"] = np.sin(2 * np.pi * data[column_name] / max_value)
+    data[column_name + "_cos"] = np.cos(2 * np.pi * data[column_name] / max_value)
+    return data
--- main_15.py
+++ main_15.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 
 from energy.utils import utils
@@ -5,31 +6,6 @@ from energy.utils import utils
 TF_ENABLE_ONEDNN_OPTS = 0
 
 data_name = "201501010000_202407010000_15_min"
-production_column_translation = [
-    "start_time",
-    "end_time",
-    "biomass_mwh",
-    "hydropower_mwh",
-    "wind_offshore_mwh",
-    "wind_onshore_mwh",
-    "solar_mwh",
-    "other_renewables_mwh",
-    "nuclear_mwh",
-    "lignite_mwh",
-    "hard_coal_mwh",
-    "natural_gas_mwh",
-    "pumped_storage_mwh",
-    "other_conventional_mwh",
-]
-
-consumption_column_translation = [
-    "start_time",
-    "end_time",
-    "total_load_mwh",
-    "residual_load_mwh",
-    "saved_pumped_storage_mwh",
-]
-
 
 ################################################################################
 ###################################Import Data##################################
@@ -52,10 +28,32 @@ print(f"production columns: {production_df.columns}")
 
 #### Convert the columns to a more readable format
 # Translate column names for production data
-production_df.columns = production_column_translation
+production_df.columns = [
+    "start_time",
+    "end_time",
+    "biomass_mwh",
+    "hydropower_mwh",
+    "wind_offshore_mwh",
+    "wind_onshore_mwh",
+    "solar_mwh",
+    "other_renewables_mwh",
+    "nuclear_mwh",
+    "lignite_mwh",
+    "hard_coal_mwh",
+    "natural_gas_mwh",
+    "pumped_storage_mwh",
+    "other_conventional_mwh",
+]
 
 # Translate column names for consumption data
-consumption_df.columns = consumption_column_translation
+consumption_df.columns = [
+    "start_time",
+    "end_time",
+    "total_load_mwh",
+    "residual_load_mwh",
+    "saved_pumped_storage_mwh",
+]
+
 
 # Convert numeric columns in production data
 for col in production_df.columns[2:]:
@@ -102,3 +100,66 @@ data = pd.merge(production_df, consumption_df, on=["start_time", "end_time"])
 
 # pring the data head
 print(f"Data head: {data.head()}")
+
+
+###################################Data Cleaning###################################
+# check if there is any NaN values in the data
+print(f"Check NaN values: {data.isnull().sum()}")
+
+# replace the NaN values with 0
+data.fillna(0, inplace=True)
+
+
+# check it again
+print(f"Check NaN values after replacing: {data.isnull().sum()}")
+
+# calculate total production energy
+data["total_production_mwh"] = data[
+    [
+        "biomass_mwh",
+        "hydropower_mwh",
+        "wind_offshore_mwh",
+        "wind_onshore_mwh",
+        "solar_mwh",
+        "other_renewables_mwh",
+        "nuclear_mwh",
+        "lignite_mwh",
+        "hard_coal_mwh",
+        "natural_gas_mwh",
+        "pumped_storage_mwh",
+        "other_conventional_mwh",
+    ]
+].sum(axis=1)
+
+
+###################################Data Transforming###################################
+# extract granular time information from the date_time in the data
+data["hour"] = data["start_time"].dt.hour
+data["minute"] = data["start_time"].dt.minute
+data["day_of_week"] = data["start_time"].dt.dayofweek
+data["day_of_year"] = data["start_time"].dt.dayofyear
+data["week_of_year"] = data["start_time"].dt.isocalendar().week
+
+# encode the date and time features in a way that captures the periodicity of the data
+# capture the cyclical nature of hours in a day.
+utils.encode_feature(data, "hour", 24)
+# capture the cyclical nature of minutes within each hour.
+utils.encode_feature(data, "minute", 60)
+# capture the cyclical nature of days within a week.
+utils.encode_feature(data, "day_of_week", 7)
+# capture the cyclical nature of days within a year.
+utils.encode_feature(data, "day_of_year", 365)
+# capture the cyclical nature of weeks within a year.
+utils.encode_feature(data, "week_of_year", 52)
+
+# print the data after transformation
+print(f"Data after transformation: {data.head()}")
+
+# create a "transformed" folder if not existed and save the data there
+# check if the folder exists in the data directory
+if not os.path.exists("data/transformed"):
+    os.makedirs("data/transformed")
+    print("Directory 'transformed' created")
+
+# save the data to the transformed folder
+data.to_csv(rf"data/transformed/transformed_{data_name}.csv", index=False)
